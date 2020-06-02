49d7efb5ecaebc28f78485b52f122791835a6468
ian <ian14159@gmail.com>
2020-06-02 14:21:53 +0100

    update line endings

--- src/Buffer.h
+++ src/Buffer.h
@@ -16,6 +16,14 @@ namespace spmc {
  *
  * Data pushed into the buffer is expected to be of a size equal to or smaller
  * than the Buffer size.
+ *
+ * IS THIS THE FASTEST CIRCULAR BUFFER IMPLEMENTATION?
+ *
+ * Should be able to avoid % in calc below
+ *
+ * m_front = (m_front + size) % m_capacity;
+ * m_size -= size;
+ *
  */
 template <class Allocator>
 class Buffer : private Allocator
