e20483b6c42cc9e5a7be356e98f7f69d1a40184f
ian <ian14159@gmail.com>
2020-08-24 10:15:58 +0100

    Fix for modulus helper macros and simplify the code.

--- src/detail/Utils.h
+++ src/detail/Utils.h
@@ -2,13 +2,19 @@
 #define IPC_DETAIL_UTILS_H
 
 #if defined(__GNUC__)
-    #define SPMC_COND_EXPECT(exp,c) (__builtin_expect((exp), c))
-    #define SPMC_EXPECT_TRUE(exp)   (__builtin_expect((exp) == true, false))
-    #define SPMC_EXPECT_FALSE(exp)  (__builtin_expect((exp) == false, true))
+    #define SPMC_COND_EXPECT(expr,c) (__builtin_expect((expr), c))
+    #define SPMC_EXPECT_TRUE(expr)   (SPMC_COND_EXPECT((expr), 1))
+    #define SPMC_EXPECT_FALSE(expr)  (SPMC_COND_EXPECT((expr), 0))
 #else
-    #define SPMC_COND_EXPECT (exp,c) ((exp) == c)
-    #define SPMC_EXPECT_TRUE  (exp)  ((exp) == true,  false)
-    #define SPMC_EXPECT_FALSE (exp)  ((exp) == false), true)
+    #define SPMC_COND_EXPECT(expr,c) ((expr) == c)
+    #define SPMC_EXPECT_TRUE(expr)   (SPMC_COND_EXPECT((expr), 1))
+    #define SPMC_EXPECT_FALSE(expr)  (SPMC_COND_EXPECT((expr), 0))
+#endif
+
+#if defined SPMC_MODULUS
+#define MODULUS(number, divisor) (number - (divisor * (number / divisor)))
+#else
+#define MODULUS(number, divisor) (number % divisor)
 #endif
 
 #endif // IPC_DETAIL_UTILS_H
