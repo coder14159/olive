4b828e479ea1bb237f9471e61648f2690725946b
ian <ian14159@gmail.com>
2021-01-14 15:38:08 +0000

    - Add environment variables for managing compile jobs

--- scripts/build_run_spsc_pgo.sh
+++ scripts/build_run_spsc_pgo.sh
@@ -9,15 +9,18 @@ bin_dir=build/x86_64/bin
 profile_bin_dir=build/x86_64.pgo_profile/bin
 release_bin_dir=build/x86_64.pgo_release/bin
 
+# Use environment variable "JOBS" to set the build job count
+jobs=${JOBS:-1}
+
 echo "# Build executable for removing shared memory"
-cd $base_dir && make ${bin_dir}/remove_shared_memory
+cd $base_dir && make ${bin_dir}/remove_shared_memory --jobs $jobs
 
 echo "# Removing shared memory $memory_name"
 cd $base_dir && ${bin_dir}/remove_shared_memory --names $memory_name
 
 echo "# Build executables for generating profile data"
-cd $base_dir && make PGO_PROFILE=1 -j2 $profile_bin_dir/spsc_server
-cd $base_dir && make PGO_PROFILE=1 -j2 $profile_bin_dir/spsc_client
+cd $base_dir && make PGO_PROFILE=1 $profile_bin_dir/spsc_server --jobs $jobs
+cd $base_dir && make PGO_PROFILE=1 $profile_bin_dir/spsc_client --jobs $jobs
 
 echo "# Run the tests generating profile guiding data"
 cd $base_dir && $profile_bin_dir/spsc_server --cpu 1 --name $memory_name --message_size 32 --queue_size 20480 --rate 0 --clients 1&
@@ -34,9 +37,9 @@ pkill spsc_server
 echo "# Removing shared memory $memory_name"
 cd $base_dir && ${bin_dir}/remove_shared_memory --names $memory_name
 
-echo "# Rebuild profile guided release"
-cd $base_dir && make PGO_RELEASE=1 -j2 $release_bin_dir/spsc_server
-cd $base_dir && make PGO_RELEASE=1 -j2 $release_bin_dir/spsc_client
+echo "# Build profile guided release"
+cd $base_dir && make PGO_RELEASE=1 $release_bin_dir/spsc_server --jobs $jobs
+cd $base_dir && make PGO_RELEASE=1 $release_bin_dir/spsc_client --jobs $jobs
 
 echo "# Run the tests using profile guided data"
 cd $base_dir && $release_bin_dir/spsc_server --cpu 1 --name $memory_name --message_size 32 --queue_size 20480 --rate 0 --clients 1&
