08fabfbcfe14c7c441629db417c0776e9fdbe4c0
ian <ian14159@gmail.com>
2021-01-14 15:25:20 +0000

    - Add environment variables for makefile jobs and command line variables

--- scripts/build_run_spmc_pgo.sh
+++ scripts/build_run_spmc_pgo.sh
@@ -9,23 +9,34 @@ bin_dir=build/x86_64/bin
 profile_bin_dir=build/x86_64.pgo_profile/bin
 release_bin_dir=build/x86_64.pgo_release/bin
 
+# Use environment variables to override default options
+jobs=${JOBS:-1}
+prefetch_size=${PREFETCH_SIZE:-0}
+queue_size=${QUEUE_SIZE:-20480}
+
 echo "# Build executable for removing shared memory"
-cd $base_dir && make ${bin_dir}/remove_shared_memory
+cd $base_dir && make ${bin_dir}/remove_shared_memory --jobs $jobs
 
 echo "# Removing shared memory $memory_name"
 cd $base_dir && ${bin_dir}/remove_shared_memory --names $memory_name
 
 echo "# Build executables for generating profile data"
-cd $base_dir && make PGO_PROFILE=1 -j2 $profile_bin_dir/spmc_server
-cd $base_dir && make PGO_PROFILE=1 -j2 $profile_bin_dir/spmc_client
+cd $base_dir && make PGO_PROFILE=1 $profile_bin_dir/spmc_server --jobs $jobs
+cd $base_dir && make PGO_PROFILE=1 $profile_bin_dir/spmc_client --jobs $jobs
 
 echo "# Run the tests generating profile guiding data"
-cd $base_dir && $profile_bin_dir/spmc_server --cpu 1 --name $memory_name --message_size 32 --queue_size 20480 --rate 0&
+cd $base_dir && \
+   $profile_bin_dir/spmc_server --cpu 1 --name $memory_name \
+                                --message_size 32 --queue_size $queue_size \
+                                --rate 0&
 
 sleep 2
 
-# cd $base_dir && ./$profile_bin_dir/spmc_client --cpu 3 --name $memory_name --log_level INFO&
-cd $base_dir && ./$profile_bin_dir/spmc_client --cpu 2 --name $memory_name --log_level INFO --stats latency,throughput,interval&
+cd $base_dir && \
+./$profile_bin_dir/spmc_client --cpu 2 --name $memory_name \
+                               --log_level INFO \
+                               --stats latency,throughput,interval \
+                               --prefetch_cache $prefetch_size&
 
 sleep 15
 
@@ -35,17 +46,23 @@ pkill spmc_server
 echo "# Removing shared memory $memory_name"
 cd $base_dir && ${bin_dir}/remove_shared_memory --names $memory_name
 
-echo "# Rebuild profile guided release"
-cd $base_dir && make PGO_RELEASE=1 -j2 $release_bin_dir/spmc_server
-cd $base_dir && make PGO_RELEASE=1 -j2 $release_bin_dir/spmc_client
+echo "# Build profile guided release"
+cd $base_dir && make PGO_RELEASE=1 $release_bin_dir/spmc_server --jobs $jobs
+cd $base_dir && make PGO_RELEASE=1 $release_bin_dir/spmc_client --jobs $jobs
 
 echo "# Run the tests using profile guided data"
-cd $base_dir && $release_bin_dir/spmc_server --cpu 1 --name $memory_name --message_size 32 --queue_size 20480 --rate 0&
+cd $base_dir && \
+   $release_bin_dir/spmc_server --cpu 1 --name $memory_name \
+                                --message_size 32 --queue_size $queue_size \
+                                --rate 0&
 
 sleep 2
 
-# cd $base_dir && ./$release_bin_dir/spmc_client --cpu 3 --name $memory_name --log_level INFO&
-cd $base_dir && ./$release_bin_dir/spmc_client --cpu 2 --name $memory_name --log_level INFO --stats latency,throughput,interval&
+cd $base_dir && \
+./$release_bin_dir/spmc_client --cpu 2 --name $memory_name \
+                               --log_level INFO \
+                               --stats latency,throughput,interval \
+                               --prefetch_cache $prefetch_size&
 
 sleep 15
 
