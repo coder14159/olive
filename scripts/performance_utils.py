29b24199113e604c878845b2390b3ec21b065f67
ian <ian14159@gmail.com>
2021-05-08 19:33:11 +0100

    - Update output directory path - Add function for pretty printing of throughput

--- scripts/performance_utils.py
+++ scripts/performance_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import pathlib
+from pathlib import Path
 
 #
 # Update the CPU bind list to use -1 for clients which do not bind to a cpu
@@ -14,21 +14,39 @@ def cpu_bind_list (cpu_list, client_count):
 
   return cpu_list
 
-#
-# Return a unique path if directory path already exists
-#
-def output_directory (base_directory, server_rate, message_size, client_count):
+# Return a directory based on the parameters supplied
+def output_directory_path (
+  base_directory,
+  server_queue_size, server_rate, server_message_size,
+  client_count, client_prefetch_size):
 
-  base_directory = pathlib.Path (base_directory)       \
-            / "server_rate"  / str (server_rate)  \
-            / "message_size" / str (message_size) \
-            / "client_count" / str (client_count)
+  server_rate_str = 'max' if server_rate is '0' else str (server_rate)
 
-  i = 0
-  directory = base_directory
-  while pathlib.Path (directory).exists ():
-      i = i + 1
-      directory = pathlib.Path (base_directory) / ("v" + str (i))
+  return Path (base_directory) \
+            / 'server_queue_size'    / str (server_queue_size) \
+            / 'server_rate'          / server_rate_str  \
+            / 'server_message_size'  / str (server_message_size) \
+            / 'client_count'         / str (client_count) \
+            / 'client_prefetch_size' / str (client_prefetch_size)
 
-  return directory
+# Return a human readable message throughput string
+def throughput_messages_to_pretty (rate):
+    if rate == 'max':
+        return rate
+    if rate == '0':
+        return 'max'
 
+    K = 1.0e3
+    M = 1.0e6
+    G = 1.0e6
+
+    float_rate = float (rate)
+
+    if float_rate > G:
+        return str (round (float_rate / G, 1)) + " G msgs/s"
+    if float_rate > M:
+        return str (round (float_rate / M, 1)) + " M msgs/s"
+    if float_rate > K:
+        return str (round (float_rate / K)) + " K msgs/s"
+
+    return str (round (float_rate)) + " msgs/s"
