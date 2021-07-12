aa1eeecacf949e073efd6b22f768b7c84cef564b
ian <ian14159@gmail.com>
2021-07-12 16:16:27 +0100

    - Minor formatting changes to SPMCStream header - Comment out the Buffer class since it is no longer required.   DataReange class gives higher throughput and lower latency

--- src/SPMCStream.h
+++ src/SPMCStream.h
@@ -75,19 +75,18 @@ private:
 
 private:
 
-  bool m_registered = { false };
-
-  std::unique_ptr<QueueType> m_queuePtr;
-
+  alignas (CACHE_LINE_SIZE)
   bool m_stop = { false };
 #pragma message "is atomic stop boolean required?"
   // std::atomic<bool> m_stop = { false };
 
   detail::ConsumerState m_consumer;
 
+  std::unique_ptr<QueueType> m_queuePtr;
+
   QueueType &m_queue;
 
-  Buffer<std::allocator<uint8_t>> m_cache;
+  // Buffer<std::allocator<uint8_t>> m_cache;
 };
 
 /*
