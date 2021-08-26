765fee37528949459986e5fc7b28dc2345688f75
ian <ian14159@gmail.com>
2021-08-26 11:09:00 +0100

    - Make pgo script run time configurable from command line - Remove prefetch cache configuration as no longer required

--- scripts/build_run_spmc_pgo.sh
+++ scripts/build_run_spmc_pgo.sh
@@ -7,7 +7,7 @@ cur_dir=`pwd`
 # Use environment variables to set command line parameters
 jobs=${JOBS:-1}
 queue_size=${QUEUE_SIZE:-20480}
-prefetch_size=${PREFETCH_SIZE:-0}
+run_time=${RUN_TIME:-17}
 
 memory_name=test_memory_spmc
 bin_dir=build/x86_64/bin
@@ -31,18 +31,16 @@ cd $base_dir && \
                                 --message_size 32 --queue_size $queue_size \
                                 --rate 0&
 
-sleep 2
-
+sleep 1
 cd $base_dir && \
    $profile_bin_dir/spmc_client --cpu 2 --name $memory_name \
                                 --log_level INFO \
-                                --stats latency,throughput,interval \
-                                --prefetch_size $prefetch_size&
+                                --stats latency,throughput,interval&
 
-sleep 15
+sleep $run_time
 
-pkill spmc_client
 pkill spmc_server
+pkill spmc_client
 
 echo "# Removing shared memory $memory_name"
 cd $base_dir && ${bin_dir}/remove_shared_memory --names $memory_name
@@ -60,16 +58,16 @@ cd $base_dir && \
 
 sleep 2
 
+sleep 1
 cd $base_dir && \
-   $release_bin_dir/spmc_client --cpu 2 --name $memory_name \
+   $release_bin_dir/spmc_client --cpu 3 --name $memory_name \
                                 --log_level INFO \
-                                --stats latency,throughput,interval \
-                                --prefetch_size $prefetch_size&
+                                --stats latency,throughput,interval&
 
-sleep 15
+sleep $run_time
 
-pkill spmc_client
 pkill spmc_server
+pkill spmc_client
 
 echo "# Removing shared memory $memory_name"
 cd $base_dir && ${bin_dir}/remove_shared_memory --names $memory_name
